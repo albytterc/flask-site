1d35903916e3f4699b380087138b2e80b653baf3
Albert Terc <albytterc@gmail.com>
2022-07-12 21:28:45 +0000

    made path source relative

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,6 @@
 import unittest, sys
 
-sys.path.append('/home/codio/workspace/flask-site') # imports python file from parent directory
+sys.path.append('../flask-site') # imports python file from parent directory
 from main import app #imports flask app object
 
 class BasicTests(unittest.TestCase):
