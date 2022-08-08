fc6e8b93f9a6f29eba69c2cc9c595e46263b629f
Albert Terc <albytterc@protonmail.com>
2022-08-08 11:32:58 -0400

    renamed package and added to gitignore

--- src/model.py
+++ src/model.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from flasksite import app
+from src import app
 
 db = SQLAlchemy(app)
 
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,7 @@
 import unittest, sys
 
 sys.path.append('../flask-site') # imports python file from parent directory
-from flasksite import app #imports flask app object
+from src import app #imports flask app object
 
 class BasicTests(unittest.TestCase):
 
